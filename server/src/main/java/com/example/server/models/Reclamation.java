4c483be67cd30beb8e817929082c625069a3cf40
Anass Saidi <anas.15.saidi@gmail.com>
2022-09-23 15:34:53 +0100

    Fixing update issues

--- server/src/main/java/com/example/server/models/Reclamation.java
+++ server/src/main/java/com/example/server/models/Reclamation.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class Reclamation {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(name = "nom")
